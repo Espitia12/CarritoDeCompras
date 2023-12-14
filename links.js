d7dbdd00aa46ed83aba53fd76b56baa2b18c57bb
johan <johanespita@gmail.com>
2023-12-13 19:19:56 -0500

    organizando cosas2

---
+++ links.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    document.querySelector(".buttonAdmin").addEventListener("click", function () {
+
+        window.location.href = "admin/admin.html";
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    document.querySelector(".buttonUsuario").addEventListener("click", function () {
+
+        window.location.href = "../index.html";
+    });
+});
